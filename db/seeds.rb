03a666dd80d96b4d639894b0f22d3cba5933f792
Josephine Bromley <bromleyjosephine@gmail.com>
2021-01-26 20:34:18 +0000

    created the seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Restaurant #{restaurant.id} has been created"
+end
+
+puts "Done"
